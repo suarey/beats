26ad1f0be2680bc0a14b7038fc6dd2381e7a66c8
David Suarez <dsuarey@gmail.com>
2018-10-30 16:29:49 -0700

    moved and updated server.js, package.json, package-lock

---
+++ server.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+
+const
+	express = require('express'),
+	app = express(),
+	logger = require('morgan'),
+	mongoose = require('mongoose'),
+	MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/react-jwt',
+	PORT = process.env.PORT || 4000,
+	usersRoutes = require('./routes/users.js')
+	sequencesRoutes = require('./routes/sequences.js')
+	
+
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, (err) => {
+	console.log(err || `Connected to MongoDB.`)
+})
+
+// Express Configuration
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'))
+app.use(express.json())
+
+app.get('/api', (req, res) => {
+	res.json({message: "API root."})
+})
+
+app.use('/api/users', usersRoutes)
+app.use('/api/sequences', sequencesRoutes)
+
+app.listen(PORT, (err) => {
+	console.log(err || `Server running on port ${PORT}.`)
+})
