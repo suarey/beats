596a97bfbce0358f1b07489cdbc28a66cc92b8da
David Suarez <dsuarey@gmail.com>
2018-10-30 14:04:47 -0700

    all sounds addded, css on index updated, back end almost full crud

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,37 +9,26 @@ class Home extends Component {
     state = {
         playing: "false"
     }
+    // playKick = (event) => {
+    //     let samplePathKick = '/assets/kick.wav';
+    //     let audio = new Audio(samplePathKick)
+    //      audio.play()
+    // }
+    // playSnare = (event) => {
+    //     let samplePathSnare = '/assets/snare.wav';
+    //     let audio = new Audio(samplePathSnare)
+    //      audio.play()
+    // }
 
-    playAudio = (event) => {
-        let samplePath = '/assets/clap-1.wav';
+    playSample = (event) =>{
+        console.log("EVENT", event.target.getAttribute("data-sample"))
+        let sample = event.target.getAttribute("data-sample");
+        let samplePath = `/assets/${sample}.wav`;
         let audio = new Audio(samplePath)
          audio.play()
-
-        // return new Promise((resolve, reject) => {
-        //     let AudioContext = window.AudioContext || window.webkitAudioContext;
-        //     let audioCtx = new AudioContext();
-        //     console.log(audioCtx)
-        //     let audioElement = document.querySelector('audio');
-        //     let track = audioCtx.createMediaElementSource(audioElement);
-        //     track.connect(audioCtx.destination);
-        //     // check if context is in suspended state (autoplay policy)
-        //     if (audioCtx.state === 'suspended') {
-        //         audioCtx.resume();
-        //     }
-        //     if (event.dataset.playing === 'false') {
-        //         audioElement.play();
-        //         event.dataset.playing = 'true';
-        //     } else if (event.dataset.playing === 'true') {
-        //         audioElement.pause();
-        //         event.dataset.playing = 'false';
-        //     }
-
-        //     resolve("Completed")
-        // }) 
     }
-
     handleClick = async (e) => {
-        
+
         // let button = e.target;
         // await this.playAudio(button)
         // button.dataset.playing = "false";
@@ -51,15 +40,58 @@ class Home extends Component {
     render() {
         return ( 
             <div>
-                <Header text={"Welcome to BEATS.909"}/>
-                <button name="clap-1" onClick={this.playAudio} role="switch" aria-checked="false">
-                    Play/Pause
+                <Header text={"PUSH MY BUTTONS!"}/>
+                <div className="button-wrapper">
+                <button  name="clap-1" data-sample="clap-1" onClick={this.playSample} role="switch" aria-checked="false">
+                    CLAP
                 </button>
-                 {/* <button name="clap-1" onClick={this.handleClick} data-playing="false" role="switch" aria-checked="false">
-                    Play/Pause
-                </button> */}
-                {/* Audio Files */}
-               {/* <audio src="/assets/clap-1.wav" name="clap-1" type="audio/wav"></audio> */}
+
+                   <button  name="kick" data-sample="kick" onClick={this.playSample} role="switch" aria-checked="false">
+                    KICK
+                </button>
+                <button  name="snare" data-sample="snare" onClick={this.playSample} role="switch" aria-checked="false">
+                    SNARE
+                </button>
+                <button  name="cowbell" data-sample="cowbell" onClick={this.playSample} role="switch" aria-checked="false">
+                    COWBELL
+                </button>
+                <button  name="cymbal" data-sample="cymbal" onClick={this.playSample} role="switch" aria-checked="false">
+                    CYMBAL
+                </button>
+                <button  name="closed-hihat" data-sample="hihat-close" onClick={this.playSample} role="switch" aria-checked="false">
+                    CLOSED HI-HAT
+                </button>
+                <button  name="kick2" data-sample="kick2" onClick={this.playSample} role="switch" aria-checked="false">
+                    KICK2
+                </button>
+                <button  name="ride" data-sample="ride" onClick={this.playSample} role="switch" aria-checked="false">
+                    RIDE
+                </button>
+                <button  name="open-hi-hat" data-sample="hihat-open-1" onClick={this.playSample} role="switch" aria-checked="false">
+                    OPEN HI-HAT
+                </button>
+                <button name="rim" data-sample="rim" onClick={this.playSample} role="switch" aria-checked="false">
+                    RIM
+                </button>
+                <button name="tom-h" data-sample="tom-h" onClick={this.playSample} role="switch" aria-checked="false">
+                    HI-TOM
+                </button>
+                <button name="tom-l" data-sample="tom-l" onClick={this.playSample} role="switch" aria-checked="false">
+                    LOW-TOM
+                </button>
+                <button name="crash" data-sample="crash" onClick={this.playSample} role="switch" aria-checked="false">
+                    CRASH
+                </button>
+                <button name="snare2" data-sample="SNARE2" onClick={this.playSample} role="switch" aria-checked="false">
+                    SNARE2
+                </button>
+                <button name="clap2" data-sample="clap2" onClick={this.playSample} role="switch" aria-checked="false">
+                    CLAP2
+                </button>
+                <button name="midtom" data-sample="midtom" onClick={this.playSample} role="switch" aria-checked="false">
+                    MID-TOM
+                </button>
+                </div>
             </div>
         )
     }
--- src/components/common/Navbar/Navbar.js
+++ src/components/common/Navbar/Navbar.js
@@ -6,8 +6,8 @@ export default ({ currentUser }) => {
     return (
         <nav className="nav clearfix">
             <div className="float-left">
-                <span className="nav-link">BEATS.909</span>
-                <Link className="nav-link" to="/">Home</Link>
+                <span ><a href="https://imgur.com/2iHabfd"><img className="max-width: 50%" src="https://i.imgur.com/2iHabfd.png" title="source: imgur.com" /></a></span>
+                <Link className="nav-link" to="/">HOME</Link>
                 {/* <Link className="nav-link" to="/vip">VIP</Link> */}
             </div>
             <div className="float-right">
@@ -15,13 +15,13 @@ export default ({ currentUser }) => {
                     ? (
                         <span>
                             <span className="nav-link">Welcome {currentUser.name}</span>
-                            <Link className="nav-link" to="/logout">Logout</Link>
+                            <Link className="nav-link" to="/logout">LOGOUT</Link>
                         </span>        
                     )
                     : (
                         <span>
-                            <Link className="nav-link" to="/login">Login</Link>
-                            <Link className="nav-link" to="/signup">Signup</Link>           
+                            <Link className="nav-link" to="/login">LOGIN</Link>
+                            <Link className="nav-link" to="/signup">SIGNUP</Link>           
                         </span>
                     )
                 }
